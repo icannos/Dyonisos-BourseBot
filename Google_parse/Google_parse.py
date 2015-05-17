ed447403267895fc6153e5b0d4bc0bffb017be0c
icannos <maxD-dev@live.fr>
2015-05-17 13:23:53 +0200

    New Firms DB + GoogleFinanceAPI

--- Google_parse/Google_parse.py
+++ Google_parse/Google_parse.py
@@ -1,1 +1,24 @@
 __author__ = 'Atelier'
+
+from appclass import AutoApp
+import json
+
+# This Class is used only to grab financial info from the Google API and store it in the database,
+# there is no reflexion in this class
+
+class Google_Parse():
+
+    api_url = 'http://finance.google.com/finance/info?q=EPA:'
+    data = {}
+
+
+
+    def __init__(self):
+        pass
+
+    def get_stock_quote(self, ticker_symbol):
+        url = self.api_url + ticker_symbol
+        lines = urllib2.urlopen(url).read().splitlines()
+        self.data[ticker_symbol] = json.loads(''.join([x for x in lines if x not in ('// [', ']')]))
+        return self.data
+
